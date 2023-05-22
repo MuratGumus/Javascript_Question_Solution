4f7777b514053ce6aee36f7f043e1c3c74171d29
gumusmuratgm@gmail.com <gumusmuratgm@gmail.com>
2023-05-22 16:12:39 +0300

    first lesson ended

--- practise.js
+++ practise.js
@@ -1,1 +1,113 @@
-console.log("helloooooo");
+console.log("Hello World!");
+
+// let number = 0;
+
+// if (number>0) {
+//     console.log("number is positive");
+// }else if (number<0) {
+//     console.log("number is negative");
+// }else{
+//     console.log("number is zero");
+// }
+
+
+// const number = 4;
+
+// if (number%2==0) {
+//     console.log("number is even");
+// }else{
+//     console.log("number is odd");
+// }
+
+
+
+
+// const number = +prompt("please enter your number")
+
+// if (number==1) {
+//     console.log("pazartesi");
+// } else if (number==2) {
+//     console.log("sali");
+// } else if (number==3) {
+//     console.log("carsamba");
+// }else if (number==4) {
+//     console.log("persembe");
+// }else if (number==5) {
+//     console.log("cuma");
+// }else if (number==6) {
+//     console.log("cumartesi");
+// }else if (number==7) {
+//     console.log("pazar");
+// }else{
+//     console.log("please enter your number correctly");
+// }
+
+// ? 18 yaşından büyük, araba kullanabilmek
+
+// let age = +prompt("please enter your age")
+// let canDrive = +prompt("1-0");
+
+// if (age>=18 && canDrive==true) {
+//     console.log("get a license");
+// }else if (age>=18 && canDrive==false) {
+//     console.log("you have learn to drive");
+// }else if (age<18 && canDrive==true) {
+//     console.log("You are to young. sabrett");
+// }else{
+//     console.log(":) :) :) ");
+// }
+
+// TODO LOGİN CHECK
+
+// const id = 123123;
+// const password = 123123;
+
+// const userID = +prompt("please enter your id")
+// const userPassword = +prompt("Please enter your password")
+
+// if (userID==id && userPassword==password) {
+//     console.log("login succesfull");
+// }else if (userID==id && userPassword!=password) {
+//     console.log("id is correct, password is wrong");
+// }else if (userID!=id && userPassword==password) {
+//     console.log("id is wrong, password is correct");
+// }else{
+//     console.log("login failed");
+// }
+
+// let realId = 123123;
+// let realPassword = 123123;
+
+// let id = +prompt("please enter your id")
+// let password;
+
+// if (realId==id) {
+//     console.log("id is correct");
+//     password = +prompt("Please enter your password");
+//     if (realPassword==password) {
+//         console.log("password is correct");
+//         console.log("Login succesfull");
+//     }else{
+//         console.log("password is wrong");
+//     }
+// }else {
+//     console.log("wrong id.");
+// }
+
+
+// let calculate = +prompt("Press 1- to add \nPress 2- to subs \nPress 3- multi\nPress 4 to division")
+// let number1 = +prompt("Please enter a number1")
+// let number2 = +prompt("Please enter a number2")
+
+
+// if (calculate==1) {
+//     console.log(number1+number1);
+// }else if (calculate==2) {
+//     console.log(number1-number2);
+// }else if (calculate==3) {
+//     console.log(number1*number2);
+// }else if (calculate==4) {
+//     console.log(number1/number2);
+// }else{
+//     console.log("please enter your calculate correctly");
+// }
